72c8d425b3fa1d06860f497fbd56e53ecb0deeba
Nerlylli <katenerlycadet@gmail.com>
2026-01-13 16:24:27 -0500

    Created Qu5-6-7 files

--- task-1/js/qu4.js
+++ task-1/js/qu4.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//VARIABLES
 let fill1 = [114, 197, 242];
 let fill2 = [58, 136, 232];
 let fill3 = [20, 46, 79];
@@ -15,21 +16,21 @@ function setup() {
 function draw() {
     background(0);
 
-    //triangle 1
+    //rectangle 1
     push();
     noStroke();
     fill(fill1);
     rect(0, 0, 200, height);
     pop();
 
-    //triangle 2
+    //rectangle 2
     push();
     noStroke();
     fill(fill2);
     rect(200, 0, 200, height);
     pop();
 
-    //triangle 3
+    //rectangle 3
     push();
     noStroke();
     fill(fill3);
@@ -37,7 +38,11 @@ function draw() {
     pop();
 }
 
+
 function mouseMoved() {
+    /**IF-ELSE STATEMENT*/
+
+    //rectangle 1
     if (mouseX >= 0 && mouseX <= 200) {
         fill1 = [255, 255, 255];
     }
@@ -45,6 +50,7 @@ function mouseMoved() {
         fill1 = [114, 197, 242];
     }
 
+    //rectangle 2
     if (mouseX >= 200 && mouseX <= 400) {
         fill2 = [255, 255, 255];
     }
@@ -52,6 +58,7 @@ function mouseMoved() {
         fill2 = [58, 136, 232];
     }
 
+    //rectangle 3
     if (mouseX >= 400 && mouseX <= 600) {
         fill3 = [255, 255, 255];
     }
---
+++ task-1/js/qu5.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function setup() {
+    console.log("go")
+
+
+    createCanvas(600, 600);
+    background(0);
+
+
+
+}
+
+function draw() {
+}
+
