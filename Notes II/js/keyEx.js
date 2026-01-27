b9fd91d8ba777a26f315149ce6fe5a0206c80e1c
Nerlylli <katenerlycadet@gmail.com>
2026-01-27 16:48:34 -0500

    finished KeyEvents

--- Notes II/js/keyEx.js
+++ Notes II/js/keyEx.js
@@ -1,12 +1,28 @@
 window.onload = function () {
     console.log("keys");
 
+    let speedX = 5;
+    let boxA = document.querySelector("#boxA");
+
     window.addEventListener("keydown",
         function (e) {
 
-            //body of callback
+            if (e.key === "ArrowRight") {
+                console.log(parseInt(boxA.style.left))
+                let currentPos = parseInt(boxA.style.left)
+                boxA.style.left = currentPos + speedX + "px"
+            }
+            if (e.key === "ArrowLeft") {
+                console.log(parseInt(boxA.style.left))
+                let currentPos = parseInt(boxA.style.left)
+                boxA.style.left = currentPos - speedX + "px"
+            }
+
+
+            //body of callback function
+            // console.log(e);
+            // console.log(e.key);
+            // console.log(e.code);
         }
     )
-
-
 }
